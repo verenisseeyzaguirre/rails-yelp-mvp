77e1681ad1102b9c596129ba44105645bcc52754
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-19 14:32:48 -0500

    rating only integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, :inclusion => 0..5
+  validates :rating, presence: true, numericality: { only_integer: true }, :inclusion => 0..5
 end
