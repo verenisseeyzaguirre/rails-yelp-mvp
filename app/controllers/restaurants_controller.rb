218b6f2ce45570f1f43644f8c29a7926af414924
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-19 15:51:12 -0500

    install gem tester

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,21 +1,25 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: %i[show edit update destroy]
   def index
     @restaurants = Restaurant.all
   end
-  def show
-  end
+
+  def show; end
+
   def new
     @restaurant = Restaurant.new
   end
+
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurants_path
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @restaurant.update(restaurant_params)
