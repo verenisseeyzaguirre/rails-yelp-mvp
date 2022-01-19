f5a782714e35cc29781e26076496da5bdb14a12e
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-19 14:34:03 -0500

    defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    collection do
+      # 'resturants/top' otra ruta
+      # get :top
+    end
+    member do
+      # 'resturants/1/chef'
+      # get :chef
+    end
+    resources :reviews, only: [:new, :create]
+  end
 end
