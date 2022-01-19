8e858941c48a9089d036459662d1c697b72b3aac
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-19 14:32:12 -0500

    populate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning...'
+Restaurant.destroy_all
+
+puts 'Creating'
+tanta = { name: 'Tanta', address: 'Lima 1', phone_number: '1234', category: 'french' }
+pardos = { name: 'Pardos', address: 'Lima 2', phone_number: '5678', category: 'belgian' }
+tusan = { name: 'Tusan', address: 'Lima 3', phone_number: '9123', category: 'chinese' }
+cuatro = { name: 'Cuatro', address: 'Lima 4', phone_number: '4444', category: 'italian' }
+katana = { name: 'Katana', address: 'Lince', phone_number: '5555', category: 'japanese' }
+
+[tanta, pardos, tusan, cuatro, katana].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
+puts 'Finished!'
