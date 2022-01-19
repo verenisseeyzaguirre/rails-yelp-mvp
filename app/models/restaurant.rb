2cbb66c40745a71849d0afef5b7e5a34e0a5c808
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-19 13:19:20 -0500

    models created w validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, acceptance: { accept: CATEGORIES }
+end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, :inclusion => 0..5
+end
